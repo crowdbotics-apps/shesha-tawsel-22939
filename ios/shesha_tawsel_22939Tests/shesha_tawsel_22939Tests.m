aaf3efeedcd6c0ae103dd35bce788aec25475010
Crowdbotics <team@crowdbotics.com>
2020-11-24 01:31:57 +0000

    Initial commit.

---
+++ ios/shesha_tawsel_22939Tests/shesha_tawsel_22939Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface shesha_tawsel_22939 : XCTestCase
+
+@end
+
+@implementation shesha_tawsel_22939
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
